530077985aad64c1612394395102321cc8e00879
homonculus <homonculus@homonculuss-Laptop.local>
2024-06-20 07:51:33 +0545

    calculator ui done

--- calculator/lib/screens/homepage.dart
+++ calculator/lib/screens/homepage.dart
@@ -14,36 +14,53 @@ class Homepage extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               //Output Display
-              Text(
-                "985",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 64,
-                    fontWeight: FontWeight.bold),
-              ),
-              //input Display
-              Container(
-                width: double.infinity,
-                padding: EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: Color(0xff21242E),
-                ),
-                child: Text(
-                  "985",
-                  textAlign: TextAlign.right,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 40,
-                  ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.2,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      "985",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 64,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    //input Display
+                    Container(
+                      width: double.infinity,
+                      padding: EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: Color(0xff21242E),
+                      ),
+                      child: Text(
+                        "985",
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 40,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
               SizedBox(height: 16),
               Expanded(
-                child: GridView(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 4,
-                  ),
+                child: Wrap(
+                  spacing: 8,
+                  runSpacing: 8,
                   children: [
+                    CalculatorNumpadItemWIdget(
+                      value: "AC",
+                      colSpan: 2,
+                    ),
+                    CalculatorNumpadItemWIdget(
+                      value: "%",
+                    ),
+                    CalculatorNumpadItemWIdget(
+                      value: "/",
+                    ),
                     CalculatorNumpadItemWIdget(
                       value: "7",
                     ),
@@ -54,7 +71,41 @@ class Homepage extends StatelessWidget {
                       value: "9",
                     ),
                     CalculatorNumpadItemWIdget(
-                      value: "X",
+                      value: "x",
+                    ),
+                    CalculatorNumpadItemWIdget(
+                      value: "4",
+                    ),
+                    CalculatorNumpadItemWIdget(
+                      value: "5",
+                    ),
+                    CalculatorNumpadItemWIdget(
+                      value: "6",
+                    ),
+                    CalculatorNumpadItemWIdget(
+                      value: "-",
+                    ),
+                    CalculatorNumpadItemWIdget(
+                      value: "1",
+                    ),
+                    CalculatorNumpadItemWIdget(
+                      value: "2",
+                    ),
+                    CalculatorNumpadItemWIdget(
+                      value: "3",
+                    ),
+                    CalculatorNumpadItemWIdget(
+                      value: "+",
+                    ),
+                    CalculatorNumpadItemWIdget(
+                      value: "0",
+                      colSpan: 2,
+                    ),
+                    CalculatorNumpadItemWIdget(
+                      value: ".",
+                    ),
+                    CalculatorNumpadItemWIdget(
+                      value: "=",
                     ),
                   ],
                 ),
@@ -68,17 +119,26 @@ class Homepage extends StatelessWidget {
 }
 
 class CalculatorNumpadItemWIdget extends StatelessWidget {
-  const CalculatorNumpadItemWIdget({super.key, required this.value});
-
+  const CalculatorNumpadItemWIdget({
+    super.key,
+    required this.value,
+    this.colSpan = 4,
+  });
+  final int colSpan;
   final String value;
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Color(0xff373F4C),
+      width: (MediaQuery.of(context).size.width / colSpan) - 8,
+      height: (MediaQuery.of(context).size.height / 8),
       child: Center(
         child: Text(
           value,
-          style: TextStyle(color: Colors.white, fontSize: 40),
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 40,
+          ),
         ),
       ),
     );
